4d41c5cfb1484b8d801f1384199acf7d3b40c34a
Romain <delasouchere@et.esiea.fr>
2015-07-09 12:28:59 +0700

    Update

--- test.py
+++ test.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python2.7
 #-*- coding: utf-8 -*-
 
-import serial
-import time
+import sensalib
+import numpy as np
 
-ser = serial.Serial(
-	port='/dev/ttyUSB0',
-	baudrate=230400,
-	parity=serial.PARITY_NONE,
-	stopbits=serial.STOPBITS_ONE,
-	bytesize=serial.EIGHTBITS
-)
-
-if ser.isOpen():
-	print "port OPEN "
-
-#print ser
-
-
-#13EAa00
-ser.write("13EAa00")
-ser.write("\r")
-
-i = 0
-
-while i < 35 :
-	print ser.read(20)
-	i += 1
+s = sensalib.Sensacell("/dev/ttyUSB0")
+s.write("13EAa00")
