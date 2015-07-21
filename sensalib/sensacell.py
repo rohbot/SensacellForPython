e1ac68ce496ae64a1a460c4c8f1997658b23bb6d
Romain <delasouchere@et.esiea.fr>
2015-07-21 17:53:49 +0700

    moduleDisplay and fullDisplay : done.

--- sensalib/sensacell.py
+++ sensalib/sensacell.py
@@ -7,9 +7,9 @@ import time
 import numpy as np
 import io
 import pickle
+from sensalib.util import *
 
 class Sensacell:
-
 	def __init__(self, port):
 		self.__name = "Sensacell"
 		self.__serUSB = serial.Serial(
@@ -21,7 +21,10 @@ class Sensacell:
 			stopbits=serial.STOPBITS_ONE,
 			timeout = 10
 		)
-		self._ser = io.TextIOWrapper(io.BufferedRWPair(self.__serUSB,self.__serUSB,1),
+		self.__serUSB.flush
+		self.__serUSB.flushInput
+		self.__serUSB.flushOutput
+		self.__ser = io.TextIOWrapper(io.BufferedRWPair(self.__serUSB,self.__serUSB,1),
 							newline = '\r',
 							line_buffering = True)
 		self.__colorArray = []
@@ -29,6 +32,7 @@ class Sensacell:
 		self.__addressList = {}
 		self.__height = 0
 		self.__width = 0
+		self.__nbModules = 0
 
 	def setSerial(self, port):
 		self.__serUSB = serial.Serial(
@@ -50,7 +54,7 @@ class Sensacell:
 		self.__write("13EAa00\r")
 		line = ""
 		while line != "01b00\r":
-			line = self._ser.readline()
+			line = self.__ser.readline()
 			if len (line) == 9:
 				if line[:1]=='0' or line[:1]=='1':
 					x=4*(int(line[4:6],16)-1)
@@ -60,7 +64,7 @@ class Sensacell:
 					if y/4 > self.__height :
 						self.__height = y/4 + 1
 					self.__addressList[int(line[6:8],16)] = [x,y]
-
+		self.__nbModules = self.__width * self.__height
 		self.__colorArray = np.zeros((self.__height*4, self.__width*4))
 		self.__sensorArray = np.zeros((self.__height*4, self.__width*4))
 		file = open(filename, 'wb')
@@ -79,17 +83,76 @@ class Sensacell:
 		self.__addressList = pickle.load(file)
 		self.__colorArray = np.zeros((self.__height*4, self.__width*4))
 		self.__sensorArray = np.zeros((self.__height*4, self.__width*4))
+		self.__nbModules = self.__width * self.__height
 		file.close()
 
 		print "Height :", self.__height, " Width :", self.__width
 		print "End Of fileAddressing, data loaded from", filename
 
+	def setColor(self, color, x, y):
+		self.__colorArray[y][x] = color
+		#print self.__colorArray
+
+	def moduleDisplay(self, address):
+		self.__write("0101a%0.2X\r"%address)
+		x = self.__addressList[address][0]
+		y = self.__addressList[address][1]
+		for i in range(y,y+4):
+			for j in range(x,x+4):
+				self.__write(util.intToByte(self.__colorArray[i][j]))
+
+	def fullDisplay(self):
+		self.__write("01%0.2Xa01\r"%self.__nbModules)
+		for i in range (1, self.__nbModules + 1):
+			x = self.__addressList[i][0]
+			y = self.__addressList[i][1]
+			for i in range(y,y+4):
+				for j in range(x,x+4):
+					self.__write(util.intToByte(self.__colorArray[i][j]))
+
 
 	def __write(self, str):
-		self._ser.write(unicode(str))
+		self.__serUSB.flushInput()
+		self.__serUSB.flush()
+		self.__serUSB.write(str)
 
 	def getColorArray(self):
 		return self.__colorArray
 
+	def setColorArray(self,array):
+		self.__colorArray = array
+
 	def getSensorArray(self):
-		return self.__sensorArray
+		return np.copy(self.__sensorArray)
+
+	def getNbModules(self):
+		return self.__nbModules
+
+	def getAddress(self,x ,y):
+		coord = [x - x%4, y - y%4]
+		for key in self.__addressList.keys():
+			if coord == self.__addressList[key]:
+				return key
+
+	def moduleListenning(self, address):
+		self.__write("r%0.2X\r"%address)
+		print ("r%0.2X\r"%address)
+		line = self.__ser.readline()
+		print line
+		self.__updateSensorModule(line,address)
+
+	def __updateSensorModule(self, line, address):
+		x = self.__addressList[address][0]
+		y = self.__addressList[address][1]
+		binaryLine =  "{0:b}".format(int(line,16),"0").zfill(16)
+		k = 0
+		for i in range(y,y+4):
+			for j in range(x,x+4):
+				self.__sensorArray[i][j] = int(binaryLine[k])*(0xFF0000)
+				k+=1
+
+	def  fullListenning(self):
+		self.__write("00%0.2Xa01\r"%self.__nbModules)
+		line = self.__ser.readline()
+		for i in range(1,self.__nbModules+1):
+			self.__updateSensorModule(line[(i-1)*4:((i-1)*4)+4],i)
--- test.py
+++ test.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python2.7
 #-*- coding: utf-8 -*-
 
+import sys
 import sensalib
 import numpy as np
 from sensalib.util import *
 
-s = sensalib.Sensacell("/dev/ttyUSB0")
+port = str(sys.argv[1])
 
-s.setSerial("/dev/ttyUSB0")
-
-s.autoAddressing("config.txt")
-
+#"/dev/ttyUSB1"
+s = sensalib.Sensacell(port)
 s.fileAddressing("config.txt")
-
-
-
-
-
-
-
-
-"""
-s.write("0101a01\r");
-color = 0x0000
-fourtab = np.full((4,4), color)
-fourtab[3,3] = 0xFFFFFF
-fourtab[2,2] = 0xFFFF
-for line in fourtab:
-	for element in line:
-		s.write(util.intToByte(element))
-"""
+s.setColor(0xFF0000,0,0)
+s.fullDisplay()
+s.moduleListenning(13)
+#s.setColorArray(s.getSensorArray())
+#s.fullDisplay()
